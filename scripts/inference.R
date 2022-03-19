4b5061bc98fdd81bece91927affab34b6ac50196
Felipe Figueiredo <philsf79@gmail.com>
2022-03-18 21:15:20 -0300

    protótipo (teste t)

--- scripts/inference.R
+++ scripts/inference.R
@@ -3,39 +3,50 @@
 
 # tables ------------------------------------------------------------------
 
+# mensuracoes como variaveis / avaliador como observacoes
+tab_inf <- analytical_long %>%
+  pivot_wider(names_from = mens, values_from = outcome) %>%
+  tbl_summary(include = 4:8, by = posicao) %>%
+  add_p()
+
+# avaliadores como variaveis / mensuracao como observacoes
+tab_inf_2 <- analytical_long %>%
+  pivot_wider(names_from = avaliador, values_from = outcome) %>%
+  tbl_summary(include = 4:8, by = posicao)
+
 # template p-value table
-tab_inf <- analytical %>%
-  tbl_summary(
-    include = c(group, outcome),
-    by = group,
-  ) %>%
-  # include study N
-  add_overall() %>%
-  # pretty format categorical variables
-  bold_labels() %>%
-  # bring home the bacon! (options: add_p or add_difference)
-  # add_p: quick and dirty
-  add_p(
-    # use Fisher test (defaults to chi-square)
-    test = all_categorical() ~ "fisher.test",
-    # use 3 digits in pvalue
-    pvalue_fun = function(x) style_pvalue(x, digits = 3),
-  ) %>%
-  # # diff: alternative to add_p
-  #   add_difference(
-  #     test = all_categorical() ~ "fisher.test",
-  #     # use 3 digits in pvalue
-  #     pvalue_fun = function(x) style_pvalue(x, digits = 3),
-  #     # ANCOVA
-  #     # adj.vars = c(sex, age, bmi),
-  #   ) %>%
-  # # EN
-  # modify_footnote(update = c(estimate, ci, p.value) ~ "ANCOVA (adjusted by sex, age and BMI)") %>%
-  # # PT
-  # modify_header(estimate ~ '**Diferença ajustada**') %>%
-  # modify_footnote(update = c(estimate, ci, p.value) ~ "ANCOVA (ajustada por sexo, idade e IMC)") %>%
-  # bold significant p values
-  bold_p()
+# tab_inf <- analytical %>%
+#   tbl_summary(
+#     include = c(group, outcome),
+#     by = group,
+#   ) %>%
+#   # include study N
+#   add_overall() %>%
+#   # pretty format categorical variables
+#   bold_labels() %>%
+#   # bring home the bacon! (options: add_p or add_difference)
+#   # add_p: quick and dirty
+#   add_p(
+#     # use Fisher test (defaults to chi-square)
+#     test = all_categorical() ~ "fisher.test",
+#     # use 3 digits in pvalue
+#     pvalue_fun = function(x) style_pvalue(x, digits = 3),
+#   ) %>%
+#   # # diff: alternative to add_p
+#   #   add_difference(
+#   #     test = all_categorical() ~ "fisher.test",
+#   #     # use 3 digits in pvalue
+#   #     pvalue_fun = function(x) style_pvalue(x, digits = 3),
+#   #     # ANCOVA
+#   #     # adj.vars = c(sex, age, bmi),
+#   #   ) %>%
+#   # # EN
+#   # modify_footnote(update = c(estimate, ci, p.value) ~ "ANCOVA (adjusted by sex, age and BMI)") %>%
+#   # # PT
+#   # modify_header(estimate ~ '**Diferença ajustada**') %>%
+#   # modify_footnote(update = c(estimate, ci, p.value) ~ "ANCOVA (ajustada por sexo, idade e IMC)") %>%
+#   # bold significant p values
+#   bold_p()
 
 # Template Cohen's D table (obs: does NOT compute p)
 # tab_inf <- analytical %>%
