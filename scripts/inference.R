08ff09db80fe227c0ee58747eb67c4940c2cc338
Felipe Figueiredo <philsf79@gmail.com>
2022-05-18 18:25:50 -0300

    reorganização: função my_icc movida para section setup

--- scripts/inference.R
+++ scripts/inference.R
@@ -1,6 +1,25 @@
 # setup -------------------------------------------------------------------
 library(irr)
 
+my_icc <- function(data, var) {
+  val <- data %>%
+    select(id, avaliador, posicao, {{var}}) %>%
+    pivot_wider(names_from = avaliador:posicao, values_from = {{var}}) %>%
+    select(-id) %>%
+    icc(type = "ag")
+  tibble(
+    metric = var_label(data, unlist = TRUE)[{{var}}],
+    val[c("value", "lbound", "ubound", "p.value")] %>% as_tibble(),
+  ) %>%
+    transmute(
+      metric,
+      # across(c(value, lbound, ubound), ~ style_number(.x, digits = 2)),
+      icc = style_number(value, digits = 2),
+      ic = format.interval(c(lbound, ubound)),
+      p.value = style_pvalue(p.value)
+    )
+}
+
 # tables ------------------------------------------------------------------
 
 # mensuracoes como variaveis / avaliador como observacoes
@@ -14,25 +33,6 @@ tab_inf <- analytical %>%
   # add_difference(test = everything() ~"paired.t.test", group = id)
   add_p(test = everything() ~"paired.t.test", group = id)
 
-my_icc <- function(data, var) {
-  val <- data %>%
-    select(id, avaliador, posicao, {{var}}) %>%
-    pivot_wider(names_from = avaliador:posicao, values_from = {{var}}) %>%
-    select(-id) %>%
-    icc(type = "ag")
-  tibble(
-    metric = var_label(data, unlist = TRUE)[{{var}}],
-    val[c("value", "lbound", "ubound", "p.value")] %>% as_tibble(),
-    ) %>%
-    transmute(
-      metric,
-      # across(c(value, lbound, ubound), ~ style_number(.x, digits = 2)),
-      icc = style_number(value, digits = 2),
-      ic = format.interval(c(lbound, ubound)),
-      p.value = style_pvalue(p.value)
-    )
-}
-
 tab_icc <- rbind(
   my_icc(analytical, "c"),
   my_icc(analytical, "rot1"),
@@ -40,54 +40,3 @@ tab_icc <- rbind(
   my_icc(analytical, "phisitiku"),
   my_icc(analytical, "zwipp")
   )
-
-# template p-value table
-# tab_inf <- analytical %>%
-#   tbl_summary(
-#     include = c(group, outcome),
-#     by = group,
-#   ) %>%
-#   # include study N
-#   add_overall() %>%
-#   # pretty format categorical variables
-#   bold_labels() %>%
-#   # bring home the bacon! (options: add_p or add_difference)
-#   # add_p: quick and dirty
-#   add_p(
-#     # use Fisher test (defaults to chi-square)
-#     test = all_categorical() ~ "fisher.test",
-#     # use 3 digits in pvalue
-#     pvalue_fun = function(x) style_pvalue(x, digits = 3),
-#   ) %>%
-#   # # diff: alternative to add_p
-#   #   add_difference(
-#   #     test = all_categorical() ~ "fisher.test",
-#   #     # use 3 digits in pvalue
-#   #     pvalue_fun = function(x) style_pvalue(x, digits = 3),
-#   #     # ANCOVA
-#   #     # adj.vars = c(sex, age, bmi),
-#   #   ) %>%
-#   # # EN
-#   # modify_footnote(update = c(estimate, ci, p.value) ~ "ANCOVA (adjusted by sex, age and BMI)") %>%
-#   # # PT
-#   # modify_header(estimate ~ '**Diferença ajustada**') %>%
-#   # modify_footnote(update = c(estimate, ci, p.value) ~ "ANCOVA (ajustada por sexo, idade e IMC)") %>%
-#   # bold significant p values
-#   bold_p()
-
-# Template Cohen's D table (obs: does NOT compute p)
-# tab_inf <- analytical %>%
-#   # select
-#   select(
-#     -id,
-#   ) %>%
-#   tbl_summary(
-#     by = group,
-#   ) %>%
-#   add_difference(
-#     test = all_continuous() ~ "cohens_d",
-#     # ANCOVA
-#     adj.vars = c(sex, age, bmi),
-#   ) %>%
-#   modify_header(estimate ~ '**d**') %>%
-#   bold_labels()
